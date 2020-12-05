013adfb0c1b0999d1482900e0f41901ed76109ac
panbihong <1527276579@qq.com>
2020-12-05 11:43:47 +0800

    fix：修改多线程运行测试用例的方法2

--- Test/Case/allTestRunThread.py
+++ Test/Case/allTestRunThread.py
@@ -7,10 +7,11 @@ import threading
 def Atest1():
     # 测试用例存放路径
     case_path = 'E:/yim-test/Test/Case'
+    # 筛选测试用例
     discover = unittest.defaultTestLoader.discover(case_path, pattern="test_xiaoxi_qunliao*.py")
     suite = unittest.TestSuite()
     suite.addTest(discover)
-
+    # 运行测试用例
     timestr = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))  # 本地日期时间作为测试报告的名字
     filename = 'e:/report/' + 'APP功能第二轮测试结果' + timestr + '.html'  # 这个路径改成自己的目录路径
     fp = open(filename, 'wb')
@@ -29,9 +30,11 @@ def Atest1():
 def Atest2():
     # 测试用例存放路径
     case_path = 'E:/yim-test/Test/Case'
+    # 筛选测试用例
     discover = unittest.defaultTestLoader.discover(case_path, pattern="test_xiaoxi_siliao*.py")
     suite = unittest.TestSuite()
     suite.addTest(discover)
+    # 运行测试用例
     timestr = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))  # 本地日期时间作为测试报告的名字
     filename = 'e:/report/' + 'APP功能第二轮测试结果' + timestr + '.html'  # 这个路径改成自己的目录路径
     fp = open(filename, 'wb')
